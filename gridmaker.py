2bdb8b1e29ab9ff05504b0ea6e40f0899b8ffd8a
sirvenny <joshvenn80@gmail.com>
2018-05-02 20:18:57 +0100

    added button to get entry text

--- gridmaker.py
+++ gridmaker.py
@@ -8,6 +8,10 @@ frame2 = Frame(top)
 frame.pack(side = LEFT)
 frame2.pack(side = RIGHT)
 
+#set default value
+colour = StringVar()
+colour.set('defaut')
+
 
 #Colour Hex input
 colour_input_txt = Label(frame, text="Colour Hex code")
@@ -16,11 +20,15 @@ colour_entry = Entry(frame, bd =5)
 colour_entry.pack(side = TOP, padx = 5, pady = 5)
 
 #go button
-generate = tkinter.Button(frame, text="Generate!")
+def gen_colour():
+    colour.set(colour_entry.get())
+    print(colour)
+
+
+generate = tkinter.Button(frame, text="Generate!", command=gen_colour)
 generate.pack(side = BOTTOM, padx = 5, pady = 5)
 
-
-label1 = Label(frame, text=colour_entry.get())
+label1 = Label(frame, textvariable=colour)
 label1.pack(side = TOP, padx = 5, pady = 5)
 
 #canvas
@@ -32,4 +40,4 @@ arc = grid.create_arc(coord, start=0, extent=150, fill="red")
 grid.pack(side = RIGHT)
 
 
-top.mainloop()
+frame.mainloop()
