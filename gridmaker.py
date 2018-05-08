e921369021a0f6aa64aad313f127570adf4f84f5
sirvenny <joshvenn80@gmail.com>
2018-05-09 00:33:42 +0100

    1) Added ability to create grid with black crossing lines (grid), number of lines depending on user input. 2) created reset button to delete all old lines. 3) FRM class to create root window, added title to window. 4) canvas size set accoring to variables canvasx and canvasy. 5) Set default value for colour as #ffffff (white). 6) Removed labels indicating number of lines and colour

--- gridmaker.py
+++ gridmaker.py
@@ -2,43 +2,63 @@ import tkinter
 from tkinter import *
 
 #creates frame
-top = Tk()
-frame = Frame(top)
-frame2 = Frame(top)
+
+class FRM(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+root = Tk()
+app = FRM(master=root)
+app.master.title("Gridmaker")
+
+frame = Frame(root)
+frame2 = Frame(root)
 frame.pack(side = LEFT)
 frame2.pack(side = RIGHT)
 
-#set default value
-colour = StringVar()
-colour.set('#ffffff')
+#set default values
+colour = StringVar(root, value = '#ffffff')
+num_lines = IntVar()
+canvasx = 300
+canvasy = 300
 
 #Colour Hex input
 colour_input_txt = Label(frame, text="Colour Hex code")
 colour_input_txt.pack(side = TOP, padx = 5, pady = 5)
-colour_entry = Entry(frame, bd =5)
+colour_entry = Entry(frame, bd =5, textvariable=colour)
 colour_entry.pack(side = TOP, padx = 5, pady = 5)
 
+#Number of lines input
+num_lines_label = Label(frame, text="Number of Lines")
+num_lines_label.pack(side = TOP, padx = 5, pady = 5)
+num_lines_entry = Entry(frame, bd =5)
+num_lines_entry.pack(side = TOP, padx = 5, pady = 5)
+
+
 #go button
-def gen_colour():
-    colour.set(colour_entry.get())
-    grid.configure(bg=colour.get())
+def generate():
+    canvas.configure(bg=colour.get())
+    num_lines = int((num_lines_entry.get()))
 
+    for i in range(num_lines + 1):
+        horiz_lines = canvas.create_line(i*(canvasx/(num_lines+1)), 0, i*(canvasx/(num_lines+1)), canvasy)
+        vert_lines = canvas.create_line(0, i*(canvasy/(num_lines+1)), canvasx, i*(canvasy/(num_lines+1)))
 
+# Reset Button
+def reset():
+    canvas.delete("all")
+    canvas.update()
 
-generate = tkinter.Button(frame, text="Generate!", command=gen_colour)
-generate.pack(side = BOTTOM, padx = 5, pady = 5)
+#Buttons
+reset_btn = tkinter.Button(frame, text="Reset lines", command=reset)
+reset_btn.pack(side = BOTTOM, padx = 5, pady = 5)
 
-label1 = Label(frame, textvariable=colour)
-label1.pack(side = TOP, padx = 5, pady = 5)
+generate_btn = tkinter.Button(frame, text="Generate!", command=generate)
+generate_btn.pack(side = BOTTOM, padx = 5, pady = 5)
 
 #canvas
-grid = tkinter.Canvas(frame2, bg=colour.get(), height=300, width=300)
-
-coord = 10, 50, 240, 210
-grid.create_line(100, 0, 100, 300)
-
-
-grid.pack(side = RIGHT)
+canvas = tkinter.Canvas(frame2, bg=colour.get(), height=canvasy, width=canvasx)
+canvas.pack(side = RIGHT)
 
 
 frame.mainloop()
