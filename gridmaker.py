dd517f3c7daf61bab68f4ade1eb0bc691ab91266
sirvenny <joshvenn80@gmail.com>
2018-05-09 19:10:24 +0100

    Added grid colour entry

--- gridmaker.py
+++ gridmaker.py
@@ -18,19 +18,29 @@ frame2.pack(side = RIGHT)
 
 #set default values
 colour = StringVar(root, value = '#ffffff')
+grid_colour = StringVar(root, value = '#000000')
 num_lines = IntVar()
 canvasx = 300
 canvasy = 300
 
-#Colour Hex input
-colour_input_txt = Label(frame, text="Colour Hex code")
-colour_input_txt.pack(side = TOP, padx = 5, pady = 5)
+#Background Colour Hex input
+colour_input_label = Label(frame, text="Colour Hex code")
+colour_input_label.pack(side = TOP, padx = 5, pady = 5)
+
 colour_entry = Entry(frame, bd =5, textvariable=colour)
 colour_entry.pack(side = TOP, padx = 5, pady = 5)
+
+#Grid Colour Hex input
+grid_input_label = Label(frame, text="Colour Hex code")
+grid_input_label.pack(side = TOP, padx = 5, pady = 5)
+
+grid_colour_entry = Entry(frame, bd =5, textvariable=grid_colour)
+grid_colour_entry.pack(side = TOP, padx = 5, pady = 5)
 
 #Number of lines input
 num_lines_label = Label(frame, text="Number of Lines")
 num_lines_label.pack(side = TOP, padx = 5, pady = 5)
+
 num_lines_entry = Entry(frame, bd =5)
 num_lines_entry.pack(side = TOP, padx = 5, pady = 5)
 
@@ -48,8 +58,8 @@ def generate():
 
     #draw horizontal and vertical lines according to num_lines input
     for i in range(num_lines + 1):
-        horiz_lines = canvas.create_line(i*(canvasx/(num_lines+1)), 0, i*(canvasx/(num_lines+1)), canvasy)
-        vert_lines = canvas.create_line(0, i*(canvasy/(num_lines+1)), canvasx, i*(canvasy/(num_lines+1)))
+        horiz_lines = canvas.create_line(i*(canvasx/(num_lines+1)), 0, i*(canvasx/(num_lines+1)), canvasy, fill=grid_colour.get())
+        vert_lines = canvas.create_line(0, i*(canvasy/(num_lines+1)), canvasx, i*(canvasy/(num_lines+1)), fill=grid_colour.get())
 
 
 
