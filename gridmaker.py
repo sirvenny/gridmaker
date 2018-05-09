03927ea0cf9e579e5e552e53921aef173a8b5f81
sirvenny <joshvenn80@gmail.com>
2018-05-09 19:00:55 +0100

    removed reset button, generate button now resets canvas

--- gridmaker.py
+++ gridmaker.py
@@ -37,21 +37,23 @@ num_lines_entry.pack(side = TOP, padx = 5, pady = 5)
 
 #go button
 def generate():
+    #Reset canvas - delete old grid
+    canvas.delete("all")
+    canvas.update()
+    #change canvas background colour
     canvas.configure(bg=colour.get())
+
+    #retrive number of lines
     num_lines = int((num_lines_entry.get()))
 
+    #draw horizontal and vertical lines according to num_lines input
     for i in range(num_lines + 1):
         horiz_lines = canvas.create_line(i*(canvasx/(num_lines+1)), 0, i*(canvasx/(num_lines+1)), canvasy)
         vert_lines = canvas.create_line(0, i*(canvasy/(num_lines+1)), canvasx, i*(canvasy/(num_lines+1)))
 
-# Reset Button
-def reset():
-    canvas.delete("all")
-    canvas.update()
+
 
 #Buttons
-reset_btn = tkinter.Button(frame, text="Reset lines", command=reset)
-reset_btn.pack(side = BOTTOM, padx = 5, pady = 5)
 
 generate_btn = tkinter.Button(frame, text="Generate!", command=generate)
 generate_btn.pack(side = BOTTOM, padx = 5, pady = 5)
